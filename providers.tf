25e20cb259cc474062d1e8c1470d87710a65da51
Eliézer Martins <eliezer@gwmnet.com.br>
2021-08-14 10:32:33 -0300

    Convertendo Credential em Variável

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.64.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+  project = "DEFINIR"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
